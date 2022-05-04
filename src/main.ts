4aded3b6212b076d3e09f387da15303750e44017
Paloma St.Clair <palomast.clair@10-16-231-18.dynapool.wireless.nyu.edu>
2022-05-04 00:34:20 -0400

    clean up

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import './style.scss';
 import * as THREE from 'three';
-import { Raycaster, ShaderMaterial, Shading, Vector2 } from 'three';
 import Stats from 'three/examples/jsm/libs/stats.module';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { DragControls } from 'three/examples/jsm/controls/DragControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import * as DAT from 'dat.gui';
 
 let model = {
@@ -16,25 +12,18 @@ let model = {
 }
 
 let renderer: THREE.WebGLRenderer;
-let clock = new THREE.Clock();
 
-let controls: DragControls;
 let stats: any;
 
 let raycaster: THREE.Raycaster;
-// let pointerPosition: THREE.Vector2;
 
 let viewOne: ViewOne;
-let viewTwo: ViewTwo;
 
 let views: BaseView[] = [];
 
-import vertexShader from '../resources/shaders/shader.vert?raw';
-import fragmentShader from '../resources/shaders/shader.frag?raw';
-import { ViewOne } from './view/ViewOne';
+
 import { BaseView } from './view/BaseView';
-import { ViewTwo } from './view/ViewTwo';
-let shaderMat: ShaderMaterial;
+import { ViewOne } from './view/ViewOne';
 
 function main() {
 	initScene();
@@ -52,22 +41,19 @@ function initGUI() {
 	const gui = new DAT.GUI();
 	let tlSettings = {
 		position: 0,
-		play: () => { viewTwo.tl.play() },
-		pause: () => { viewTwo.tl.pause() },
+		play: () => { viewOne.tl.play() },
+		pause: () => { viewOne.tl.pause() },
 		restart: () => {
-			viewTwo.tl.pause()
-			viewTwo.tl.seek(0)
-			viewTwo.tl.play()
+			viewOne.tl.pause()
+			viewOne.tl.seek(0)
+			viewOne.tl.play()
 		},
-		newPlanet:()=>{
-			
-		}
 	}
 	const tlControls = gui.addFolder("timeline")
 	tlControls.open()
-	tlControls.add(tlSettings, "position", 0, 8, 0.01).onChange((value) => {
-		viewTwo.tl.pause()
-		viewTwo.tl.seek(value)
+	tlControls.add(tlSettings, "position", 0, 250, 0.01).onChange((value) => {
+		viewOne.tl.pause()
+		viewOne.tl.seek(value)
 	})
 	tlControls.add(tlSettings, "play")
 	tlControls.add(tlSettings, 'pause');
@@ -87,40 +73,8 @@ function initScene() {
 	viewOne = new ViewOne(model, renderer);
 	views.push(viewOne);
 
-	viewTwo = new ViewTwo(model, renderer);
-	views.push(viewTwo);
-
-	// controls = new OrbitControls(camera, renderer.domElement);
 
 	raycaster = new THREE.Raycaster();
-	// model.pointerPosition = new THREE.Vector2(0,0);
-
-	const uniforms = {
-		u_time: { type: 'f', value: 1.0 },
-		u_resolution: { type: 'v2', value: new THREE.Vector2(800, 800) },
-		// u_mouse: { type: 'v2', value: new THREE.Vector2() },
-	};
-
-	shaderMat = new THREE.ShaderMaterial({
-		uniforms: uniforms,
-		vertexShader: vertexShader,
-		fragmentShader: fragmentShader,
-		side: THREE.DoubleSide,
-	});
-
-	// add event listener to highlight dragged objects
-
-	// controls = new DragControls([plane], camera, renderer.domElement);
-
-	// controls.addEventListener('dragstart', function(event) {
-	// 	event.object.material.emissive.set(0xaaaaaa);
-	// })
-
-	// controls.addEventListener('dragend', function (event) {
-	// 	event.object.material.emissive.set(0x000000);
-	// })
-
-	// // Init animation
 	animate();
 }
 
@@ -128,47 +82,10 @@ function initListeners() {
 	window.addEventListener('resize', onWindowResize, false);
 
 	window.addEventListener('pointermove', onPointerMove);
-
-	window.addEventListener('keydown', (event) => {
-		const { key } = event;
-		// console.log(key);
-
-		switch (key) {
-			case 'e':
-				const win = window.open('', 'Canvas Image');
-
-				const { domElement } = renderer;
-
-				// Makse sure scene is rendered.
-				// renderer.render(scene, camera);
-
-				const src = domElement.toDataURL();
-
-				if (!win) return;
-
-				win.document.write(`<img src='${src}' width='${domElement.width}' height='${domElement.height}'>`);
-				break;
-
-			case 'ArrowRight':
-				model.activeView = (model.activeView + 1) % views.length
-				break;
-
-			case 'ArrowLeft':
-				model.activeView = (model.activeView - 1)
-				if (model.activeView < 0) {
-					model.activeView = views.length - 1;
-				}
-				break;
-
-			default:
-				break;
-		}
-	});
 }
 
 function onWindowResize() {
 	viewOne.onWindowResize();
-	viewTwo.onWindowResize();
 }
 
 function onPointerMove(event: any) {
@@ -181,47 +98,7 @@ function animate() {
 	requestAnimationFrame(() => {
 		animate();
 	});
-
-	let delta = clock.getDelta();
-
-	shaderMat.uniforms.u_time.value += delta;
-
-	switch (model.activeView) {
-		case 0:
-			viewOne.update(clock);
-			break;
-
-		case 1:
-			viewTwo.update(clock);
-			break;
-
-		default:
-			break;
-	}
-	
-
-
-	// raycaster.setFromCamera(pointerPosition, camera)
-	// const intersects = raycaster.intersectObjects(scene.children)
-
-	// for (let i = 0; i < scene.children.length; i++) {
-	// 	if (scene.children[i].type === "Mesh") {
-	// 		(scene.children[i] as MeshObj).material.color.set(0x888888);
-	// 	}
-	// }
-
-	// for (let i = 0; i < intersects.length; i++) {
-	// 	if (intersects[i].object.type === 'Mesh') {
-	// 		(intersects[i].object as MeshObj).material.color.set(0xff0000);
-	// 	}
-	// }
-
-
-	if (stats) stats.update();
-
-	// if (controls) controls.update();
-
-	renderer.render(views[model.activeView].scene, views[model.activeView].camera);
+	renderer.render(viewOne.scene, viewOne.camera);
 }
 
 main();
--- src/view/ViewOne.ts
+++ src/view/ViewOne.ts
@@ -1,148 +1,222 @@
-import {
-	Mesh,
-	Renderer,
-	BoxGeometry,
-	MeshPhongMaterial,
-	AmbientLight,
-	PointLight,
-	Group,
-	Material,
-	TextureLoader,
-	RepeatWrapping,
-	Texture,
-	MeshBasicMaterial,
-	WebGLRenderer,
-	PlaneBufferGeometry,
-	DoubleSide,
-	Clock
-} from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import THREE, { AmbientLight, Color, DodecahedronGeometry, DoubleSide, Group, Material, Mesh, MeshBasicMaterial, MeshPhongMaterial, OctahedronGeometry, PointLight, ShapeGeometry, SphereGeometry, TextureLoader, TorusGeometry, Vector3, WebGLRenderer } from "three";
 import { BaseView } from "./BaseView";
+import { gsap } from "gsap";
+import {OBJLoader} from 'three/examples/jsm/loaders/OBJLoader.js';
+import {MTLLoader} from 'three/examples/jsm/loaders/MTLLoader.js'
 
-export class ViewOne extends BaseView{
+export class ViewOne extends BaseView {
 
-	group: Group;
-	cube: Mesh;
-	plane: Mesh;
-	exampleModel: Group;
-	exampleTexture: Texture;
-
-	lightAmbient: AmbientLight;
+	paloma: any;
+	stars: Array<any>;
+	tl: any;
 	lightPoint: PointLight;
+	lightAmbient: AmbientLight;
+	lightPoint2: PointLight;
+	colorPalette = [0xffd700, 0xfafad2, 0xc0c0c0,0xffd700, 0xfaf0be, 0xeae0c8, 0xffd700, 0xf2f3f4, 0xf7e98e,0xc4aead, 0xf8de7e];
+	starsizes = [1, .7, 1, .5, .6, 1];
+	sizeInd = 0; 
+	colorInd = 0;
+	outsideE!: Mesh;
 
-	constructor(model: any, renderer: WebGLRenderer){
+
+	constructor(model: any, renderer: WebGLRenderer) {
 		super(model, renderer);
 
-		this.exampleModel = new Group();
-		this.exampleTexture = new Texture();
-		this.group = new Group();
-		this.scene.add(this.group);
-
-		const cubeGeometry = new BoxGeometry();
-		const cubeMaterial = new MeshPhongMaterial({ color: 0xf0bbbb });
-		// cubeMaterial.wireframe = true;
-		this.cube = new Mesh(cubeGeometry, cubeMaterial);
-		this.cube.castShadow = true;
-
-		this.group.add(this.cube);
-
-
-		const geometryPlane = new PlaneBufferGeometry(6, 6, 10, 10);
-		const materialPlane = new MeshPhongMaterial({
-			color: 0x666666,
-			side: DoubleSide,
-			flatShading: true,
-		});
-
-		this.plane = new Mesh(geometryPlane, materialPlane);
-		this.plane.position.z = -2;
-		this.plane.receiveShadow = true;
-		this.scene.add(this.plane);
-
-		this.lightAmbient = new AmbientLight(0x333333);
-		this.scene.add(this.lightAmbient);
+		this.stars = [];
+		
+		this.tl = gsap.timeline();
+		
 
 		this.lightPoint = new PointLight(0xffffff);
-		this.lightPoint.position.set(-0.5, 0.5, 4);
-		this.lightPoint.castShadow = true;
-		this.lightPoint.intensity = 0.25;
+		this.lightPoint.intensity = 0.8;
+		this.lightPoint.position.set(0, 0, -25);
+		this.lightPoint.castShadow = false;
+
+		this.lightPoint2 = this.lightPoint.clone();
+		this.lightPoint2.position.set(0, 0, 25);
+    	this.lightPoint2.intensity = .8;
+    	this.lightPoint2.castShadow = false;
+    	this.scene.add(this.lightPoint2);
+
 		this.scene.add(this.lightPoint);
 
-		const mapSize = 1024; // Default 512
-		const cameraNear = 0.5; // Default 0.5
-		const cameraFar = 500; // Default 500
-		this.lightPoint.shadow.mapSize.width = mapSize;
-		this.lightPoint.shadow.mapSize.height = mapSize;
-		this.lightPoint.shadow.camera.near = cameraNear;
-		this.lightPoint.shadow.camera.far = cameraFar;
-
-
-		let textureMaterial: Material;
-		let textureLoader = new TextureLoader().setPath('../resources/textures/');
-		textureLoader.load('uv_grid_opengl.jpg', (texture) => {
-			texture.wrapS = texture.wrapT = RepeatWrapping;
-			texture.anisotropy = this.renderer.capabilities.getMaxAnisotropy();
-
-			this.exampleTexture = texture;
-
-			textureMaterial = new MeshBasicMaterial({ map: texture });
-
-			this.cube.material = textureMaterial;
-
-			const modelLoader = new GLTFLoader().setPath('../resources/models/');
-			modelLoader.load('teapot.gltf', (gltf) => {
-				
-				this.exampleModel = gltf.scene;
-				console.log(this.exampleModel);
-
-				this.exampleModel.scale.set(0.01, 0.01, 0.01);
-				this.exampleModel.position.x = 2;
-
-				const teapotMat = new MeshPhongMaterial({ color: 0x22ff22 });
-
-				this.exampleModel.traverse((child: THREE.Object3D<THREE.Event>) => {
-					console.log(child);
-					console.log(child.type === 'Mesh');
-					if (child.type === 'Mesh') {
-						(child as gltfMesh).material = textureMaterial;
-					}
-				});
-
-				// scene.add(exampleModel)
-				this.group.add(this.exampleModel);
-			});
-		});
-	}
-
-	update(clock: Clock): void {
-
-		this.cube.rotation.x += 0.01;
-		this.cube.rotation.y += 0.01;
-
-		// group.rotateZ(delta);
-		this.group.rotation.set(0, 0, this.model.groupAngle);
-		this.group.position.set(this.model.groupX, this.model.groupY, 0);
-
-		const vertArray = this.plane.geometry.attributes.position;
-		for (let i = 0; i < vertArray.count; i++) {
-			vertArray.setZ(i, Math.sin(clock.getElapsedTime() + i - vertArray.count / 2) * 0.5 + Math.cos(clock.getElapsedTime() - i) * 0.5);
-		}
-		this.plane.geometry.attributes.position.needsUpdate = true;
-
-
-		if (this.exampleModel != undefined) {
-			this.exampleModel.rotateX(0.01);
-			this.exampleModel.rotateY(0.01);
-		}
-
-		if (this.exampleTexture) {
-			this.exampleTexture.center.set(0.5, 0.5);
-			this.exampleTexture.rotation += clock.getDelta();
-		}
+		this.lightAmbient = new AmbientLight(0xffffff);
+		this.lightAmbient.intensity = 0.2;
+	
+		this.scene.add(this.lightAmbient);
+		this.loadingObjects();
+		console.log(this.stars.length);
 		
-	}
-}
+		for (let i = 0; i < 25; i++) {
+			if (this.sizeInd>this.starsizes.length-1){this.sizeInd = 0};
+			if (this.colorInd>this.colorPalette.length-1){this.colorInd = 0};
 
-interface gltfMesh extends THREE.Object3D<THREE.Event> {
-	material: THREE.Material;
+			var octa = new OctahedronGeometry(2);
+			var material = new MeshPhongMaterial( {color:0xFFD700, shininess: 100});
+			var star = new Mesh( octa, material );
+			this.tl.to(
+				star.position, 
+					{ 
+						x: Math.cos((i / 25) * Math.PI * 2) * 20,
+						y: Math.sin((i / 25) * Math.PI * 2) * 20,
+						duration: 2,
+						ease: "bounce.out",
+						yoyo:true
+					}, `${i/4}`);
+
+			var dode = new DodecahedronGeometry(3);
+			var material = new MeshPhongMaterial( {color:0xfafad2, shininess: 100});
+			var star2 = new Mesh( dode, material );
+			this.tl.to(
+				star2.position, 
+					{ 
+						x: Math.cos((i / 25) * Math.PI * 2) * 30,
+						y: Math.sin((i / 25) * Math.PI * 2) * 30,
+						duration: 2,
+						ease: "bounce.out",
+						yoyo:true
+					}, `${i/4}`);
+			var dode = new DodecahedronGeometry(1);			
+			var material = new MeshPhongMaterial( {color:0xfafad2, shininess: 100});
+			var star5 = new Mesh( dode, material );
+			this.tl.to(
+				star5.position, 
+					{ 
+						x: Math.cos((i / 20) * Math.PI * 2) * 10,
+						y: Math.sin((i / 20) * Math.PI * 2) * 10,
+						duration: 2,
+						ease: "bounce.out",
+						yoyo:true
+					}, `${i/4}`);
+			var octa = new OctahedronGeometry(4);
+			var material = new MeshPhongMaterial( {color:0xFFD700, shininess: 100});
+			var star3 = new Mesh( octa, material );
+			this.tl.to(
+				star3.position, 
+					{ 
+						x: Math.cos((i / 25) * Math.PI *2) * 40,
+						y: Math.sin((i / 25) * Math.PI *2) * 40,
+						duration: 2,
+						ease: "bounce.out",
+						yoyo:true
+					}, `${i/4}`);
+			var  dode = new DodecahedronGeometry(5);	
+			var material = new MeshPhongMaterial( {color:0xfafad2, shininess: 100});
+			var star4 = new Mesh( dode, material );
+			this.tl.to(
+				star4.position, 
+					{ 
+						x: Math.cos((i / 20) * Math.PI *2) * 50,
+						y: Math.sin((i / 20) * Math.PI *2) * 50,
+						duration: 2,
+						ease: "bounce.out",
+						yoyo:true
+					}, `${i/4}`);
+										
+			this.colorInd ++;
+			this.sizeInd ++;
+			this.scene.add(star,star2,star3,star4,star5);
+			this.stars.push(star,star2,star3,star4,star5);
+		}
+		this.stars.forEach((star:Mesh, i:number)=>{
+			this.tl.to(
+			star.position,
+			{
+				x: Math.cos((i / 30) * Math.PI * 2 - Math.PI) * 10,
+				duration: .1,
+				ease: "sine.out",
+				yoyo:true
+				},
+	
+			);
+			i++;
+		});
+		this.stars.forEach((star:Mesh, i:number)=>{
+			if(i%2 ==0){
+				this.tl.to(star.position,{
+					x: 100,
+					y: -100,
+					duration: .3,
+					stagger:.1
+				})
+				this.tl.to(star.position,{
+					x: -100,
+					y: 100,
+					duration: .3,
+					stagger:.1
+				})
+				this.tl.to( star.position , 
+					{
+					x: Math.random() * (60 - (-60)) + -60,
+					y: Math.random() * (50 - (-50)) + -50,
+					})
+			}
+			else{
+				this.tl.to(star.position,{
+					x: -100,
+					y: 100,
+					duration: .3,
+					stagger:.1
+				})
+				this.tl.to(star.position,{
+					x: 100,
+					y: -100,
+					duration: .3,
+					stagger:.1
+				})
+				this.tl.to( star.position , 
+					{
+					x: Math.random() * (60 - (-60)) + -60,
+					y: Math.random() * (50 - (-50)) + -50,
+					})
+			}
+			i++;
+		})
+
+		console.log(this.stars.length);
+	}
+	loadingObjects(){
+
+		const mtlLoader = new MTLLoader();
+    	mtlLoader.load('3dobj/paloma_standing.mtl', (mtl) => {
+        mtl.preload();
+        for (const material of Object.values(mtl.materials)) {
+            material.side = DoubleSide;
+        }
+        const objLoader = new OBJLoader();
+        objLoader.setMaterials(mtl);
+        objLoader.load('3dobj/paloma_standing.obj', (root) => {
+			this.paloma = root;
+			root.name = 'paloma';
+			root.position.y = -5;
+			root.scale.set(7,7,7);
+			root.rotateY(270);
+			root.position.z = 4
+			this.scene.add(root);
+        });
+    	});
+		var earthloader = new TextureLoader();
+		earthloader.load( '3dobj/earth.jpeg',   ( texture: any ) => {
+		var geometry = new SphereGeometry( 25);
+		var material = new MeshBasicMaterial( { map: texture } );
+		var earth = new Mesh( geometry, material );
+		earth.position.y = -30;
+		earth.position.z = 4;
+		this.outsideE = earth;
+		this.scene.add(earth)
+		});
+	
+	}
+
+	// https://stackoverflow.com/questions/51776220/animate-an-fbx-loaded-object-in-three-js
+	animate() {
+		requestAnimationFrame( this.animate );
+		// You have access to scene - Get 'new_fbx' object from it
+		var fbxObject = this.scene.getObjectByName('paloma');
+		// Make sure this object exists before accessing it
+		if (fbxObject) {
+		   fbxObject .rotation.x += 0.01;
+		}
+		this.renderer.render( this.scene, this.camera );
+	}
 }
